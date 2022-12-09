9b772d03a21b5a154f0b18921195af69c3660d91
Brian Yoder <brianyoder@gmail.com>
2022-12-08 20:24:02 -0800

    add html form, buttons, and headers

--- js/scripts.js
+++ js/scripts.js
@@ -7,27 +7,19 @@ Order.prototype.addPizza = function(pizza) {
 this.pizzas.push(pizza);
 }
 
-
 function Pizza() {
   this.toppings= [];
-  this.price = 0
+  this.size = "";
+  this.price = 0;
 }
 
 Pizza.prototype.addToppings = function(type) {
   this.toppings.push(type)
 }
 
-Pizza.prototype.addPrice = function (topping) {  
-toppingsMenu.topping += this.price
-}
 
-const toppingsMenu = {
-  "artichoke": 1.00, 
-  "bacon": 1.00,
-  "diced olives": .50,
-  "garlic": .25,
-  "pepperoni": .75,
-}
+
+
 
  
 
