d33a34a02f999fcc24a75f7a34c75e304e665b87
Brian Yoder <brianyoder@gmail.com>
2022-12-10 19:19:33 -0800

    BUGFIX: determinePrice() function now doesn't add to price when *none* is selected

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,6 @@
 //Business Logic
 
 let newOrder = new Order();
-// this will need to be called on load
 
 function Order() {
   this.pizzas = [];
@@ -13,33 +12,22 @@ Order.prototype.addPizza = function(pizza) {
 this.pizzas.push(pizza);
 }
 
-Order.prototype.determinePrice = function() {
-  
+/*Order.prototype.determinePrice = function() {
 
     for (let index = 0; index <= countTo; index += countBy) {
       countArray.push(index);
     }
     return countArray;
-  }
+  } */
 
 function Pizza(toppings, size) {
   this.toppings = toppings;
   this.size = size;
   this.price = 0;
 }
-/*let order = {
-  pizzas: [
-    {
-    pizza: 
-  ]
-}
-*/
 
 //UI Logic
 
-//we will have to create a separate addPizzaToOrder function to call within this one, 
-//this function will only create a pizza based on the user input
-//once the pizza is made, the add pizza to order function is called
 function addPizzaToOrder(event) {
   event.preventDefault();
   let orderDiv = document.getElementById("yourpizzas");
@@ -53,14 +41,13 @@ function addPizzaToOrder(event) {
   let toppingsArray = [top1, top2, top3, top4, top5];
   let pizza = new Pizza(toppingsArray, psize);
   newOrder.addPizza(pizza); 
+  determinePrice();
   let i = newOrder.counter;
   let toppings = newOrder.pizzas[i].toppings;
   let divsize = newOrder.pizzas[i].size;
-  p.innerText = divsize + " with " + toppings.join(", ");
+  p.innerText = divsize + " with " + toppings.join(", ") ;
   orderDiv.append(p);
-  determinePrice();
   newOrder.counter +=1;
-  
 }
 
 function determinePrice() {
@@ -74,30 +61,18 @@ function determinePrice() {
    else {
     newOrder.pizzas[i] += 15;
    }
-   newOrder.pizzas[i].price += newOrder.pizzas[i].toppings.length;
+   //newOrder.pizzas[i].price += newOrder.pizzas[i].toppings.length;
    let toppingsArray = newOrder.pizzas[i].toppings;
    toppingsArray.forEach(function(topping) {
     if (topping === "none") {
-      newOrder.pizzas[i].price -= 1;
+      newOrder.pizzas[i].price += 0
+    } else if (topping != "none") {
+      newOrder.pizzas[i].price +=1
     }
   })
 }
 
-    
-  
-    //add 10 dollars to that pizza
-
-
-
-// const pElement = document.createElement("p");
-// pElement.append("text");
-// const firstDiv = document.querySelector("div");
-// firstDiv.append(pElement);
-
-
 window.addEventListener("load", function (){
   document.querySelector("form#orderpizza").addEventListener("submit", addPizzaToOrder);
  // document.querySelector("form#orderpizza").addEventListener("click", provideTotal);
-  
-  
 })
