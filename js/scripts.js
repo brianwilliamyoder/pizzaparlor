1e2f7ff347bd6e10d340815b35388c6137a6b45f
Brian Yoder <brianyoder@gmail.com>
2022-12-20 17:25:49 -0800

    add function getToppingsArray

--- js/scripts.js
+++ js/scripts.js
@@ -18,53 +18,52 @@ function Pizza(toppings, size) {
   this.price = 0;
 }
 
+function determinePrice() {
+  let i = newOrder.counter; 
+  if (newOrder.pizzas[i].size === "small") {
+    newOrder.pizzas[i].price += 10;
+  } 
+   else if (newOrder.pizzas[i].size === "medium") {
+    newOrder.pizzas[i].price += 12;
+   }
+   else if (newOrder.pizzas[i].size === "large") {
+    newOrder.pizzas[i].price += 15;
+   }
+
+   newOrder.pizzas[i].price += newOrder.pizzas[i].toppings.length;
+  }
+
 //UI Logic
+
+function getToppingsArray() {
+  const checkboxes = document.querySelectorAll('.topping:checked');
+  const toppingsArray = [];
+  checkboxes.forEach(function(checkbox) {
+    toppingsArray.push(checkbox.value);
+  });
+  return toppingsArray;
+}
 
 function addPizzaToOrder(event) {
   event.preventDefault();
   let orderDiv = document.getElementById("yourpizzas");
   let p = document.createElement("p");
-  let top1 = document.getElementById("topping-1").value;
-  let top2 = document.getElementById("topping-2").value;
-  let top3 = document.getElementById("topping-3").value;
-  let top4 = document.getElementById("topping-4").value;
-  let top5 = document.getElementById("topping-5").value;
   let psize = document.getElementById("pizza-size").value;
-  let toppingsArray = [top1, top2, top3, top4, top5];
+  let toppingsArray = getToppingsArray();
   let pizza = new Pizza(toppingsArray, psize);
   newOrder.addPizza(pizza); 
   determinePrice();
   let i = newOrder.counter;
   let toppings = newOrder.pizzas[i].toppings;
   let divsize = newOrder.pizzas[i].size;
-  p.innerText = divsize + " with " + toppings.join(",  ") + " $" + newOrder.pizzas[i].price;
+  p.innerText = divsize + " " + toppings.join(",  ") + " $" + newOrder.pizzas[i].price;
   orderDiv.append(p);
   provideTotal();
   newOrder.counter +=1;
   
 }
 
-function determinePrice() {
-  let i = newOrder.counter; 
-  if (document.getElementById("pizza-size").value = "small") {
-    newOrder.pizzas[i].price += 10;
-  } 
-   else if (document.getElementById("pizza-size").value = "medium") {
-    newOrder.pizzas[i] += 12;
-   }
-   else {
-    newOrder.pizzas[i] += 15;
-   }
-   
-   let toppingsArray = newOrder.pizzas[i].toppings;
-   toppingsArray.forEach(function(topping) {
-    if (topping === "none") {
-      newOrder.pizzas[i].price += 0
-    } else if (topping != "none") {
-      newOrder.pizzas[i].price +=1
-    }
-  })
-}
+
 
 function provideTotal() {
   let p = document.getElementById("ordertotal");
@@ -73,6 +72,18 @@ function provideTotal() {
   p.innerText = "Your total cost is $" + newOrder.totalCost;
 }
 
+function finalizeOrder() {
+  let orderDiv = document.getElementById("orderdiv");
+  let h2 = document.querySelector("h2")
+  let p = document.createElement("p");
+  p.innerText = "Please pick up in 45 minutes"
+  let orderTotal = document.getElementById("orderdiv");
+  orderTotal.append(p);
+  h2.style.display = "none"
+  orderDiv.style.display = "none";
+}
+
 window.addEventListener("load", function (){
   document.querySelector("form#orderpizza").addEventListener("submit", addPizzaToOrder);
-})
+  document.querySelector("div#finish-order").addEventListener("click", finalizeOrder);
+});
